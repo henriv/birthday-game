22a1b91ee82577db9ac90a47c4c014830f0d4586
Birthday Game Deploy <birthday-game@deploy.local>
2026-03-03 10:21:52 +0200

    Fix component import path

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import BirthdayGame from '@/components/BirthdayGame';
+import BirthdayGame from '../components/BirthdayGame';
 
 export default function Home() {
   return <BirthdayGame />;
